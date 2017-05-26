12dddaa3be62c7979830994a5fbbd2771ee07611
Croydon <cr0ydon@outlook.com>
2017-05-26 21:28:42 +0200

    Fix

--- tool/travis_conditional_conan_package_generating.sh
+++ tool/travis_conditional_conan_package_generating.sh
@@ -57,11 +57,11 @@ cd ../
 
 echo "Get possible updates of appveyor.yml"
 sed 's/#CI //' inexor/appveyor.yml > appveyor.yml
-sed --in-place 's/#CIDELETE /d' appveyor.yml
+sed --in-place '/#CIDELETE /d' appveyor.yml
 
 echo "Get possible updates of .travis.yml"
 sed 's/#CI //' inexor/.travis.yml > .travis.yml
-sed --in-place 's/#CIDELETE /d' .travis.yml
+sed --in-place '/#CIDELETE /d' .travis.yml
 
 echo "Create a commit"
 git add -A
