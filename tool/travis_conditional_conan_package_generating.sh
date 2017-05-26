375244637c2abe19c9a81e663420fbf141c3271f
Croydon <cr0ydon@outlook.com>
2017-05-26 18:53:05 +0200

    More syntax

--- tool/travis_conditional_conan_package_generating.sh
+++ tool/travis_conditional_conan_package_generating.sh
@@ -10,17 +10,22 @@ set -e
 
 # Making sure we NEVER execute anything of this for pull requests as this could be a huge security risk
 if [[ "${TRAVIS_PULL_REQUEST}" != false ]]; then
+    echo "We don't build Conan packages for pull requests for security reasons."
     exit 0
 fi
 
 if ! [[ "${TRAVIS_BRANCH}" == "master" ]]; then
+    echo "This isn't the master branch"
     exit 0
 fi
 
 # Check if important files did change in the last commit
 if [[ "$(git diff --name-only HEAD^^ -- dependencies.py)" == "" ]]; then
+    echo "No changes found in Conan dependencies!"
     exit 0
 fi
+
+echo "Configure git"
 
 cd ..
 git clone --recursive https://github.com/inexor-game/ci-prebuilds.git "ci-prebuilds"
@@ -31,16 +36,20 @@ git config user.name ${GITHUB_BOT_NAME}
 git config user.email ${GITHUB_BOT_EMAIL}
 
 # Update submodule
+echo "Update submodule in ci-prebuilds"
 cd inexor
 git pull
 git checkout master
 cd ../
 
+echo "Create a commit"
 git add -A
 git commit -am "[bot] Updating Conan dependencies!"
+
 
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
 
+echo "Push commit"
 git push
 exit 0
