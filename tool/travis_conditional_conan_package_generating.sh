881d2e2a1f5e889fd48553749ad6c263e3604139
Croydon <cr0ydon@outlook.com>
2017-05-26 17:30:49 +0200

    Try auto detectiong and build stages

---
+++ tool/travis_conditional_conan_package_generating.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+# This file checks if we change files, which are important for mass building for Conan packages
+# If they changed we are going to trigger a mass building
+# We are doing this by updating the submodule in inexor-game/ci-prebuilds invoking thereby Travis/AppVeyor processes
+
+# TODO: support for branches with conan- instead of only master to test mass building of packages easily
+
+# this makes the entire script fail if one commands fail
+set -e
+
+# Making sure we NEVER execute anything of this for pull requests as this could be a huge security risk
+if [ test "${TRAVIS_PULL_REQUEST}" != false ]; then
+    exit 1
+fi
+
+if ! ["${TRAVIS_BRANCH}" == "master"]; then
+    exit 1
+fi
+
+# Check if important files did change in the last commit
+if [[ "$(git diff --name-only HEAD^^ -- dependencies.py)" == "" ]]; then
+    exit 1
+fi
+
+cd ..
+git clone --recursive https://github.com/inexor-game/ci-prebuilds.git "ci-prebuilds"
+cd "ci-prebuilds"
+git checkout master
+
+git config user.name ${GITHUB_BOT_NAME}
+git config user.email ${GITHUB_BOT_EMAIL}
+
+# Update submodule
+cd inexor
+git pull
+git checkout master
+cd ../
+
+git add -A
+git commit -am "[bot] Updating Conan dependencies!"
+
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
+
+git push
+exit 1
