c3da7b77e893c3e9ba6c4ae136f635e6e42274c9
Croydon <cr0ydon@outlook.com>
2017-06-17 03:28:01 +0200

    Re-Re-Re-Rebased x3

--- dependencies.py
+++ dependencies.py
@@ -5,17 +5,19 @@ requires = (("InexorGlueGen/0.6.0alpha@inexorgame/testing"),
             ("Boost/1.64.0@inexorgame/stable"),
             ("RapidJSON/1.0.2@inexorgame/stable"),
             ("zlib/1.2.11@lasote/stable"),
-            ("gtest/1.7.0@lasote/stable"),
+            ("gtest/1.8.0@lasote/stable"),
             ("ENet/1.3.13@inexorgame/stable"),
             ("spdlog/0.10.0@memsharded/stable"),
             ("SDL2/2.0.5@lasote/stable"),
             ("SDL2_image/2.0.1@lasote/stable"),
-            ("CEF/3.2704.1424.gc3f0a5b@a_teammate/testing")
+            ("CEF/3.2704.1424.gc3f0a5b@Croydon/testing")
         )
+
 
 options = '''
   zlib:shared=False
   gtest:shared=False
+  gtest:no_gmock=True
   ENet:shared=False
   Boost:shared=False
   SDL2:shared=False
--- tool/travis.sh
+++ tool/travis.sh
@@ -2,10 +2,7 @@
 #
 # Structure
 # * Utility functions
-# * Installation target implementations
-# * Nightly build/APIDOC uploading
 # * Compiling and testing
-# * Targets as called by .travis.yml
 # * Main routine
 
 ## UTILITY FUNCTIONS #######################################
@@ -55,70 +52,62 @@ external_pull_request() {
   fi
 }
 
-# upload remote_path local_path [local_paths]...
-#
-# Upload one or more files to nightly.inexor.org
-upload() {
-  # Fix an issue where upload directory gets specified by subsequent upload() calls
-  ncftpput -R -v -u "$FTP_USER" -p "$FTP_PASSWORD" nightly.inexor.org /linux/ "$@" || true
+
+# ACTUALLY COMPILING AND TESTING INEXOR ####################
+
+build() {
+  (
+    mkcd "/tmp/inexor-build-${build}"
+
+    conan --version
+
+    # TODO: FIXME: We need a hardcoded workaround for GCC5.4 to link gtest successfully
+    #if [[ $CC == "gcc-5" ]]; then
+        #conan install gtest/1.8.0@lasote/stable --build -s compiler="$CONAN_COMPILER" -s compiler.version="$CONAN_COMPILER_VERSION" -s compiler.libcxx="libstdc++11" -e CC="$CC" -e CXX="$CXX"
+    #fi
+
+    conan remote add inexor https://api.bintray.com/conan/inexorgame/inexor-conan
+
+    if test "$NIGHTLY" = conan; then
+      echo "executed conan install "$gitroot" --scope build_all=1 --build -s compiler=$CONAN_COMPILER -s compiler.version=$CONAN_COMPILER_VERSION -s compiler.libcxx=libstdc++11 -e CC=$CC -e CXX=$CXX"
+      conan install "$gitroot" --scope build_all=1 --build -s compiler="$CONAN_COMPILER" -s compiler.version="$CONAN_COMPILER_VERSION" -s compiler.libcxx="libstdc++11" -e CC="$CC" -e CXX="$CXX"
+    else
+      echo "executed conan install "$gitroot" --scope build_all=1 --scope create_package=1 --build=missing -s compiler=$CONAN_COMPILER -s compiler.version=$CONAN_COMPILER_VERSION -s compiler.libcxx=libstdc++11 -e CC=$CC -e CXX=$CXX"
+      conan install "$gitroot" --scope build_all=1 --scope create_package=1 --build=missing -s compiler="$CONAN_COMPILER" -s compiler.version="$CONAN_COMPILER_VERSION" -s     compiler.libcxx="libstdc++11" -e CC="$CC" -e CXX="$CXX"
+    fi
+
+    conan build "$gitroot"
+  )
 }
 
-## INSTALLATION ROUTINES ###################################
-
-install_wily_repo() {
-  echo -e "\ndeb http://archive.ubuntu.com/ubuntu wily "{main,multiverse,universe,restricted} >> /etc/apt/sources.list
+run_tests() {
+  if contains "$TARGET" linux; then
+    "${bin}/unit_tests"
+  else
+    echo >&2 "ERROR: UNKNOWN TRAVIS TARGET: ${TARGET}"
+    exit 23
+  fi
 }
 
-install_tool() {
-  apt-get -y -t trusty install ncftp
-}
-
-install_linux() {
-  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FB1BF5BF09FA0AB7
-  
-  add-apt-repository -y "deb http://ppa.launchpad.net/zoogie/sdl2-snapshots/ubuntu trusty main"
-  install_wily_repo
-  apt-get update
-  
-
-  install_tool
-
-  apt-get -y -t wily install --only-upgrade libfontconfig1
-  apt-get -y -t wily install build-essential binutils doxygen nasm
-  python -m pip install conan
-}
-
-# Install routines for each target
-
-install_win64() {
-  install_wily_repo
-  apt-get update
-  install_tool
-  apt-get -y -t wily install mingw-w64
-}
-install_win32() {
-  install_win64
-}
-install_linux_clang() {
-  install_linux
-  apt-get -y -t wily install clang-3.7 binutils
-}
-install_linux_gcc() {
-  install_linux
-  apt-get -y -t wily install gcc-5 g++-5
-}
-install_apidoc() {
-  apt-get update
-  install_tool
-  apt-get install -y -t trusty doxygen
-}
-install_osx() {
-  # if you need sudo for some stuff here, you need to adjust travis.yml and target_before_install()
-  #brew install sdl2
-  exit 0
-}
+# ATTENTION:
+# Please USE the following naming format for any files uploaded to our distribution server
+# <BRANCHNAME>-<BUILDNUMBER>-<TARGET_NAME>.EXTENSION
+# where <PACKAGENAME> is NOT CONTAINING any -
+# correct: master-1043.2-linux_gcc.txt
+# correct: refactor-992.2-apidoc.hip
+# exception: master-latest-<TARGET_NAME>.zip
+# wrong: ...-linux_gcc-1043.2.zip
 
 ## UPLOADING NIGHTLY BUILDS AND THE APIDOC #################
+
+# upload remote_path local_path [local_paths]...
+#
+# Upload one or more files to our nightly or dependencies server
+# Variables are defined on the Travis website
+upload() {
+  # Fix an issue where upload directory gets specified by subsequent upload() calls
+  ncftpput -R -v -u "$NIGHTLY_USER" -p "$NIGHTLY_PASSWORD" "$FTP_DOMAIN" / "$@"
+}
 
 upload_apidoc() {
   (
@@ -139,7 +128,7 @@ nigthly_build() {
 
   # Include the media files
   local media="${media}"
-  test -n "${media}" || test "$branch" = master && media=true
+  test -n "${media}" || test "$branch" = master && media=false
 
   echo "
   ---------------------------------------
@@ -159,42 +148,48 @@ nigthly_build() {
 
   mkdir "$outd"
 
-  if test "$media" = true; then (
-    cd "$gitroot"
-    curl -LOk https://github.com/inexor-game/data/archive/master.zip
-    unzip "master.zip" -d "$outd"
-    rm "master.zip"
-    cd "$outd"
-    mv "data-master" "media/data"
-  ) fi
+  #if test "$media" = true; then (
+    #cd "$gitroot"
+    #curl -LOk https://github.com/inexor-game/data/archive/master.zip
+    #unzip "master.zip" -d "$outd" 2>/dev/null
+    #rm "master.zip"
+    #cd "$outd"
+    #mv "data-master" "media/data"
+  #) fi
 
   local ignore="$(<<< '
     .
     ..
-    .gitignore
     build
     cmake
     CMakeLists.txt
+    Jenkinsfile
     appveyor.yml
+    conanfile.py
+    conanfile.pyc
+    dependencies.py
+    dependencies.pyc
     doxygen.conf
+    flex/.git
     .git
     .gitignore
     .gitmodules
     inexor
-    platform
+    inexor.bat
+    media/core/.git
+    server.bat
     tool
-    vendor
     .travis.yml
   ' tr -d " " | grep -v '^\s*$')"
 
   (
     cd "$gitroot"
-    ls -a | grep -Fivx "$ignore" | xargs -t cp -rvt "$outd"
+    ls -a | grep -Fivx "$ignore" | xargs -t cp -rt "$outd"
   )
 
   (
     cd "`dirname "$outd"`"
-    zip -r "$zipf" "`basename $outd`"
+    zip -r -dd -q "$zipf" "`basename $outd`"
   )
 
   (
@@ -206,42 +201,31 @@ nigthly_build() {
   upload "$zipf" "$descf"
 
   if test "$branch" = master; then (
-    ln -s "$zipf" "master-latest-$TARGET.zip"
-    ln -s "$descf" "master-latest-$TARGET.txt"
+    echo "${build}" > "master-latest-$TARGET.txt"
     upload "master-latest-$TARGET.zip" "master-latest-$TARGET.txt"
   ) fi
 
   return 0
 }
 
-# ACTUALLY COMPILING AND TESTING INEXOR ####################
-
-build() {
-  (
-    mkcd "/tmp/inexor-build-${build}"
-    conan
-    echo "executed conan install "$gitroot" --scope build_all=1 --scope create_package=1 --build=missing -s compiler=$CONAN_COMPILER -s compiler.version=$CONAN_COMPILER_VERSION -s compiler.libcxx=libstdc++11"
-    conan install "$gitroot" --scope build_all=1 --scope create_package=1  --build=missing -s compiler="$CONAN_COMPILER" -s compiler.version="$CONAN_COMPILER_VERSION" -s compiler.libcxx="libstdc++11"
-    conan build "$gitroot"
-  )
-}
-
-run_tests() {
-  if contains "$TARGET" linux; then
-    "${bin}/unit_tests"
-  elif contains "$TARGET" win; then
-    echo >&2 "Sorry, win is not supported for testing yet."
-    exit 0
-  else
-    echo >&2 "ERROR: UNKNOWN TRAVIS TARGET: ${TARGET}"
-    exit 23
-  fi
-}
-
 ## TARGETS CALLED BY TRAVIS ################################
 
-target_before_install() {
-  sudo "$script" install_"$TARGET"
+# Upload nightly
+target_after_success() {
+  if test "$TARGET" != apidoc; then
+    #external_pull_request || nigthly_build || true
+    if test "$NIGHTLY" = true; then
+        # Upload zip nightly package to our FTP
+        nigthly_build
+    fi
+    if test "$NIGHTLY" = conan; then
+        # Upload all conan packages to conan.io
+        conan user -p "${NIGHTLY_PASSWORD}" -r inexor "${NIGHTLY_USER}"
+        set -f
+        conan upload --all --force -r inexor --retry 3 --retry_wait 10 --confirm "*inexorgame*"
+        set +f
+    fi
+  fi
   exit 0
 }
 
@@ -251,39 +235,68 @@ target_script() {
   else
     build
     run_tests
-  fi
-  exit 0
-}
-
-# Upload nightly
-target_after_success() {
-  if test "$TARGET" != apidoc; then
-    external_pull_request || nigthly_build || true
+    target_after_success
   fi
   exit 0
 }
 
 ## MAIN ####################################################
 
+# this makes the entire script fail if one commands fail
 set -e
 
 script="$0"
 tool="`dirname "$0"`"
 code="${tool}/.."
 bin="${code}/bin"
+TARGET="$3"
+#CMAKE_FLAGS="$4"
+CONAN_COMPILER="$4"
+CONAN_COMPILER_VERSION="$5"
+export CC="$6"
+export CXX="$7"
 
-export main_repo="inexor-game/code"
-export branch="$TRAVIS_BRANCH" # The branch we're on
-export jobno="$TRAVIS_JOB_NUMBER" # The job number
-export commit="${TRAVIS_COMMIT}"
+export commit="$8"
+export branch="$9" # The branch we're on
+export jobno="${10}" # The job number
+# Nightly is either true, false or conan
+NIGHTLY="${11}"
+NIGHTLY_USER="${12}"
+NIGHTLY_PASSWORD="${13}"
+FTP_DOMAIN="${14}"
+
+
 # Name of this build
 export build="$(echo "${branch}-${jobno}" | sed 's#/#-#g')-${TARGET}"
-export gitroot="$TRAVIS_BUILD_DIR"
+export main_repo="inexor-game/code"
+
+# Workaround Boost.Build problem to not be able to found Clang
+if [[ $CC == clang* ]]; then
+  sudo ln -sf /usr/bin/${CC} /usr/bin/clang
+  sudo ln -sf /usr/bin/${CXX} /usr/bin/clang++
+fi
+
+# Just to make sure that no package uses the wrong GCC version...
+if [[ $CC == gcc* ]]; then
+  sudo ln -sf /usr/bin/${CC} /usr/bin/gcc
+  sudo ln -sf /usr/bin/${CXX} /usr/bin/gcc++
+fi
+
+
+if [ -z "$2" ]; then
+  export gitroot="/inexor"
+else
+  # this makes it possible to run this script successfull
+  # even if doesn't get called from the root directory
+  # of THIS repository
+  # required to make inexor-game/ci-prebuilds working
+  export gitroot="/inexor/$2"
+fi
 
 self_pull_request && {
   echo >&2 -e "Skipping build, because this is a pull " \
     "request with a branch in the main repo.\n"         \
-    "This means, there should already be a ci job for " \
+    "This means, there should already be a CI job for " \
     "this branch. No need to do things twice."
   exit 0
 }
